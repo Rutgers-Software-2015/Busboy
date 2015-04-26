dc896ca8155ffdd58ad50c1aae113b6c600c701e
David Arakelyan <da378@scarletmail.rutgers.edu>
2015-04-26 15:58:13 -0400

    Made some changes to handler and appearance of GUI
    - Removed "message" button
    - Moved things into the handler

--- Busboy_Handler.java
+++ Busboy_Handler.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.JToggleButton;
+
 import Manager.EmpObj;
 import Shared.Communicator.DatabaseCommunicator;
 
@@ -26,7 +28,33 @@ public class Busboy_Handler extends DatabaseCommunicator {
 			String temp = rs.getString("T_Status");
 			T_Status_List.add(temp);
 		}
-		//this.disconnect();
+		System.out.println(" ... done calculating");
 		return T_Status_List;
+	}
+	public void ChangeTableStatus(JToggleButton Table, int Table_ID){
+		tell("use MAINDB;");
+		tell("Select * from MAINDB.Table_Statuses Order by Table_ID;");
+		if(Table.isSelected()== true){
+			update("UPDATE MAINDB.Table_Statuses SET T_Status = 'Clean' WHERE TABLE_ID = "+Table_ID+";");
+		}
+		if(Table.isSelected()== false){
+			update("UPDATE MAINDB.Table_Statuses SET T_Status = 'Unclean' WHERE TABLE_ID = "+Table_ID+";");
+		}
+	}
+	public void initialTableStatus(JToggleButton Table,int index){
+		tell("use MAINDB;");
+		tell("Select * from MAINDB.Table_Statuses Order by Table_ID;");
+		try {
+			if (T_Status().get(index).toString().equals("Unclean")){
+				Table.setSelected(false);
+			}
+			if (T_Status().get(index).toString().equals("Clean")){
+				Table.setSelected(true);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
